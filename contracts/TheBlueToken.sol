bbce26bd7028c5859b90212064cdc062fa2491bf
Deniz Hasan <dhasan@blue-src.com>
2018-05-24 10:51:28 -0500

    Add linked list

--- contracts/TheBlueToken.sol
+++ contracts/TheBlueToken.sol
@@ -1,24 +1,31 @@
 pragma solidity ^0.4.23;
 
 import "../libs/SafeMath.sol";
+import "../libs/LibCLL.sol";
 import "./ERC20Interface.sol";
 import "./Owned.sol";
 
 contract TheBlueToken is ERC20Interface, Owned{
     using SafeMath for uint;
+    using LibCLLa for LibCLLa.CLL;
 
     string public symbol;
     string public  name;
     uint8 public decimals;
     uint public _totalSupply;
-
-    mapping(address => uint) balances;
-    constructor() Owned() public {
-        symbol = "TBT";
-        name = "The Blue Token Token";
+    
+    LibCLLa.CLL tokenslist;    
+    mapping(address => uint) tokenbalances;
+    
+    
+    constructor(string _sym, string _name) Owned() public {
+        symbol = _sym;
+        name = _name;
         decimals = 18;
-        //_totalSupply = 1000000 * 10**uint(decimals);
-        //balances[owner] = _totalSupply;
+        _totalSupply = 36000 * 10**uint(decimals);
+        
+        tokenslist.push(owner,true);
+        tokenbalances[owner] = _totalSupply;
         emit Transfer(address(0), owner, _totalSupply);
     }    
     function totalSupply() external view returns (uint supply){
@@ -26,12 +33,20 @@ contract TheBlueToken is ERC20Interface, Owned{
     }
 
     function balanceOf(address tokenOwner) external view returns (uint balance) {
-        return balances[tokenOwner];
+        return tokenbalances[tokenOwner];
     }
 
     function transfer(address to, uint tokens) external returns (bool success) {
-        balances[msg.sender] = balances[msg.sender].sub(tokens);
-        balances[to] = balances[to].add(tokens);
+        //require(tokenbalances[msg.sender] >= tokens);
+        tokenbalances[msg.sender] = tokenbalances[msg.sender].sub(tokens);
+        if (tokenbalances[msg.sender]==0){
+            tokenslist.remove(msg.sender);
+        }
+        if (tokenbalances[to]==0){
+            tokenslist.push(to,true);
+        }
+        tokenbalances[to] = tokenbalances[to].add(tokens);
+        
         emit Transfer(msg.sender, to, tokens);
         return true;
     }
