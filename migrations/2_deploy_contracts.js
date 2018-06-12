60a9f62c59c4b8f728847a4432285bd40f7f5e1b
Deniz Hasan <dhasan@blue-src.com>
2018-06-12 19:55:21 +0300

    js promise

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,12 +4,14 @@ var SafeMath = artifacts.require("../libs/SafeMath.sol");
 var BluePool = artifacts.require("./BluePool.sol");
 
     module.exports = function (deployer, network, accounts) {
-        deployer.deploy(LibCLLa, {from: accounts[0]});
-        deployer.deploy(LibCLLu, {from: accounts[0]});
-        deployer.deploy(SafeMath, {from: accounts[0]});
-        deployer.link(LibCLLa, BluePool);
-        deployer.link(LibCLLu, BluePool);
-        deployer.link(SafeMath,BluePool);
-        deployer.deploy(BluePool, 20,10,{from: accounts[0], gas:3000000});
+    	deployer.then(async () => {
+        await deployer.deploy(LibCLLa, {from: accounts[0]});
+        await deployer.deploy(LibCLLu, {from: accounts[0]});
+        await deployer.deploy(SafeMath, {from: accounts[0]});
+        await deployer.link(LibCLLa, BluePool);
+        await deployer.link(LibCLLu, BluePool);
+        await deployer.link(SafeMath,BluePool);
+        await deployer.deploy(BluePool, 20,10,{from: accounts[0], gas:3000000});
+    })
  
 };
