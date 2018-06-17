c4426c24915e73bff0f2a93d76688b8a0a92e113
Deniz Hasan <dhasan@blue-src.com>
2018-06-17 21:10:43 +0300

    fix truffle function provider

--- truffle.js
+++ truffle.js
@@ -10,7 +10,9 @@ module.exports = {
       network_id: "*"//1527617042614 // Match any network id
     },
     ropsten: {
-      provider: new HDWalletProvider(mnemonic, "https://ropsten.infura.io/aLjkBlysDHdMmhTiiyXW"),
+      provider: function() { 
+      	return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/aLjkBlysDHdMmhTiiyXW");
+      },
       gasPrice: 10000000000,
       network_id: 3
     }
